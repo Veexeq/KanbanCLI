4b04f004af6ea330b504ef0c32e860180b6ff9bd
Wiktor Trybus <wiktortrybus11@gmail.com>
2026-05-21 21:23:45 +0200

    Implemented KanbanBoard.hpp header file for the main KanbanBoard logic

---
+++ src/KanbanBoard.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "Task.hpp"
+
+class KanbanBoard {
+private:
+    std::vector<Task> m_tasks;
+    int m_next_id{1};
+
+public:
+    void addTask(const std::string& title, const std::string& description, TaskPriority priority);
+    
+    bool removeTask(int id);
+    
+    std::vector<Task> getTasksByStatus(TaskStatus status) const;
+
+    bool updateTaskStatus(int id, TaskStatus new_status);
+};
